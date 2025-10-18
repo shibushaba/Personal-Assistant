86d4eded53b8cb33946684b951bbb54f6bb68477
shabas <shibushabas23@gmail.com>
2025-10-18 15:39:43 +0530

    gh

--- main.py
+++
@@ -1,7 +0,0 @@
-import os
-import eel
-eel.init("frontend")
-os.system('start chrome.exe --app = "http://127.0.0.1:5500/frontend/index.html" ')
-
-
-eel.start("index.html",mode=None, host="localhost",block=True)
